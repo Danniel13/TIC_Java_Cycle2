6310f181bc484391af59d46d12635523804f484d
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-07-10 18:53:22 -0500

    pruebasmod

--- Class_02/jprueba.java
+++ Class_02/jprueba.java
@@ -1,3 +1,5 @@
+package Class_02;
+
 public class jprueba {
     public static void main(String[] args) {
         System.out.println("Prueba commit Toshiba");
--- Class_02/prueba2.java
+++ Class_02/prueba2.java
@@ -1,3 +1,5 @@
+package Class_02;
+
 public class prueba2 {
     public static void main(String[] args) {
         System.out.println("Lorenza!!");
