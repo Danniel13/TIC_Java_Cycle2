c0e03ec18544cd84e6bdd31cfc26bb2031ff23ac
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-07-25 18:58:27 -0500

    commit inicial

--- obdcsql/src/main/java/com/dkdev/App.java
+++ obdcsql/src/main/java/com/dkdev/App.java
@@ -1,5 +1,8 @@
 package com.dkdev;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+      //ES NECESARIO AÑADIR DEPENDENCIA DE SQLITE AL PROYECTO EN POM:
+    
+      // <!-- https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc -->
+// <dependency>
+//     <groupId>org.xerial</groupId>
+//     <artifactId>sqlite-jdbc</artifactId>
+//     <version>3.36.0.3</version>
+// </dependency>
+
+        // Creación conexión BD:
+        var urlDB = "jbdc:sqlite:C:\\Users\\D3rick\\hr.db"; //SE PONE RUTA DE BASE DE DATOS CON DOBLE BACKSLASH
+        try{//intento de conexion BD
+        var connectionDB = DriverManager.getConnection(urlDB); //Estableciendo la conexion
+        connectionDB.close();
+        } catch(SQLException e){//DEFINICION DE EXCEPCION
+          System.err.println("Error trabajando con la base de datos: \n" + e.getMessage());//ERROR EN CASO DE FALLAR CONEXION
+        }
+
+      }
     }
-}
+
